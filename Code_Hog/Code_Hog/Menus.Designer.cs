08e3fa4390453e9f6a8297a823d7f55d0853e8ce
MuazAlhaidar <omunam10@gmail.com>
2020-03-11 10:36:36 -0400

    Update Menus.Designer.cs

--- Code_Hog/Code_Hog/Menus.Designer.cs
+++ Code_Hog/Code_Hog/Menus.Designer.cs
@@ -24,40 +24,42 @@
         /// </summary>
         private void InitializeComponent() {
             this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.TicketsPage = new System.Windows.Forms.TabPage();
+            this.UserManagementPage = new System.Windows.Forms.TabPage();
             this.tabControl1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
             // 
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.TicketsPage);
+            this.tabControl1.Controls.Add(this.UserManagementPage);
             this.tabControl1.Location = new System.Drawing.Point(13, 13);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
             this.tabControl1.Size = new System.Drawing.Size(1241, 713);
             this.tabControl1.TabIndex = 0;
             // 
-            // tabPage1
+            // TicketsPage
             // 
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(1233, 687);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
-            this.tabPage1.UseVisualStyleBackColor = true;
+            this.TicketsPage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TicketsPage.Location = new System.Drawing.Point(4, 22);
+            this.TicketsPage.Name = "TicketsPage";
+            this.TicketsPage.Padding = new System.Windows.Forms.Padding(3);
+            this.TicketsPage.Size = new System.Drawing.Size(1233, 687);
+            this.TicketsPage.TabIndex = 0;
+            this.TicketsPage.Text = "Tickets";
+            this.TicketsPage.UseVisualStyleBackColor = true;
             // 
-            // tabPage2
+            // UserManagementPage
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(192, 74);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            this.UserManagementPage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UserManagementPage.Location = new System.Drawing.Point(4, 22);
+            this.UserManagementPage.Name = "UserManagementPage";
+            this.UserManagementPage.Padding = new System.Windows.Forms.Padding(3);
+            this.UserManagementPage.Size = new System.Drawing.Size(1233, 687);
+            this.UserManagementPage.TabIndex = 1;
+            this.UserManagementPage.Text = "User Management";
+            this.UserManagementPage.UseVisualStyleBackColor = true;
             // 
             // Menus
             // 
@@ -75,8 +77,8 @@
         #endregion
 
         private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage tabPage1;
-        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage TicketsPage;
+        private System.Windows.Forms.TabPage UserManagementPage;
     }
 }
 
