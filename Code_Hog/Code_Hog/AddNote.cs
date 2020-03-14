f54c5d41f4fa5a922b46268ea27f710f07653c1e
Andrew R. Davis <SgtP4in@users.noreply.github.com>
2020-03-14 12:42:31 -0400

    Created Interface for AddNote (unfinished)
    Created design and framework for the addNote Menu

---
+++ Code_Hog/Code_Hog/AddNote.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Code_Hog
+{
+    public partial class AddNote : Form
+    {
+        protected string newNote;
+
+        public AddNote()
+        {
+            InitializeComponent();
+            
+            //set the title component labels to the correct text
+            //TODO
+
+        }
+
+        //Adds note
+        private void button1_Click(object sender, EventArgs e)
+        {
+            newNote = noteTextBox.Text;
+
+            //update the ticket entry with the new note text and username
+            //TODO
+
+            //close the form
+            this.Close();
+        }
+    }
+}
